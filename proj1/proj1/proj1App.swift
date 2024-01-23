5a519f6504bb6e2432f3a43d18807d67bb1414bd
gsantella <gsantella@southhills.edu>
2024-01-23 09:39:56 -0500

    initial Commit

---
+++ proj1/proj1/proj1App.swift
@@ -0,0 +1,17 @@
+//
+//  proj1App.swift
+//  proj1
+//
+//  Created by admin on 1/18/24.
+//
+
+import SwiftUI
+
+@main
+struct proj1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
